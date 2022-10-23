398a8e67cb71038d589b17db90425e8deaf9f3fb
Tarsis de Lima <br.tmvdl@gmail.com>
2022-10-23 15:51:33 -0300

    Fixing source paths

--- env/build.sh
+++ env/build.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 
-TAGS_DIR="$(pwd)/tags"
+CURRENT_DIR=$(pwd)
 YMD=$(date +%Y%m%d)
 YM=$(date +%Y%m)
 
-TAGS=$@
-
-if [ -z $TAGS ] ; then
-  TAGS=$(ls $TAGS_DIR)
-fi
-
-echo tags $TAGS
+TAGS=$( ls src/ )
 
 for TAG in $TAGS; do
-  docker build -t tmvdl/yt-dl:$TAG $TAGS_DIR/$TAG/
+  docker build -t tmvdl/yt-dl:$TAG $CURRENT_DIR/src/$TAG/
   docker tag tmvdl/yt-dl:$TAG tmvdl/yt-dl:$TAG-$YMD
   docker tag tmvdl/yt-dl:$TAG tmvdl/yt-dl:$TAG-$YM
 done
-
